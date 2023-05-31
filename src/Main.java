c8cb90eedc1b8a76d1aa9446f051b2cf7935683a
baranbicher <baranbicer0103@outlook.com>
2023-05-31 14:18:46 +0300

    Fibonacci Sayılarını Bulan Program

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    static int fib(int n){
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+// Fibonacci Sayıları  1 1 2 3 5 8 13
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+}
